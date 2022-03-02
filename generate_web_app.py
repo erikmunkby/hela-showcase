35bb2af6c0889c0777ad893e7a7ea4a6703c0a41
Erik Munkby <erik.munkby@gmail.com>
2022-03-02 13:28:49 +0100

    fix: updated web app title

--- generate_web_app.py
+++ generate_web_app.py
@@ -1,9 +1,12 @@
 from beer_catalog import BeerCatalog
 from hela import generate_webpage
 
+generate_webpage(BeerCatalog, output_folder='.', web_app_title='BeerioCart 🍺')
+
 
 # Generate webpage and store in folder gh_pages
 generate_webpage(
     BeerCatalog, output_folder='gh_pages', overwrite_existing=True,
-    include_samples=True, web_app_title='Beer Catalog'
+    include_samples=True, web_app_title='BeerioCart 🍺'
 )
+
